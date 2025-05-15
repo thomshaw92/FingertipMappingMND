5a1a1535b3692143acfdc8013fd32c98977a021b
Michal Toth <toth.michal@ymail.com>
2025-05-15 10:06:34 +1000

    Add scripts for running recon-all-clinical on all subjects

---
+++ recon-all-clinical_run/run-recon-all-clinical.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Save the command just in case.
+CURR_CMD="$0 $@"
+# Parse arguments.
+PARSED=$(getopt --options "" --long data-dir:,output-dir:,dry-run,n-threads:,freesurfer-version:,excluded: --name "$0" -- "$@")
+# Terminate script if failed to parse arguments properly.
+if [[ $? -ne 0 ]]; then
+    echo "Error parsing options" >&2
+    exit 1
+fi
+
+# Reset the positional parameters to the parsed arguments.
+eval set -- "$PARSED"
+
+DATA_DIR="."
+OUTPUT_DIR=""
+DRYRUN=false
+# Use the max amount of available threads as the default value.
+NTHREADS=`nproc`
+FREESURFER_VERSION="8.0.0"
+EXCLUDED=()
+
+# Extract values from arguments. `--` indicates the end of arguments.
+while true; do
+    case "$1" in
+        --data-dir)
+            DATA_DIR="$2"
+            shift 2
+            ;;
+        --output-dir)
+            OUTPUT_DIR="$2"
+            shift 2
+            ;;
+        --dry-run)
+            DRYRUN=true
+            shift
+            ;;
+        --n-threads)
+            NTHREADS="$2"
+            shift 2
+            ;;
+        --freesurfer-version)
+            FREESURFER_VERSION="$2"
+            shift 2
+            ;;
+        --excluded)
+            IFS=' ' read -r -a EXCLUDED <<< "$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Unexpected option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Make sure to use the DATA_DIR value that was sepcified in the arguments.
+if [[ -z "$OUTPUT_DIR" ]]; then
+    OUTPUT_DIR="${DATA_DIR}/mri/bids/derivatives/recon-all-clinical"
+fi
+
+LOG_FILE=$OUTPUT_DIR/run-recon-all-clinical_$(date "+%Y-%m-%d_%H-%M-%S").log
+echo "$0 --data-dir $DATA_DIR --output-dir $OUTPUT_DIR --n-threads $NTHREADS --freesurfer-version $FREESURFER_VERSION --excluded ${EXCLUDED[*]}" > $LOG_FILE
+echo "" >> $LOG_FILE
+FAILED_FILE=$OUTPUT_DIR/recon-all-clinical-failed_$(date "+%Y-%m-%d_%H-%M-%S").txt
+# Make sure that each run starts with an empty failed list.
+> $FAILED_FILE
+
+for SUBJECT_DIR in "$DATA_DIR"/mri/bids/sub-*; do
+    # Get subject number.
+    SUB=$(basename "$SUBJECT_DIR")
+
+    if [[ " ${EXCLUDED[*]} " =~ " $SUB " ]]; then
+        echo "Skipping excluded subject: $SUB" | tee -a $LOG_FILE
+        continue
+    fi
+
+    for SESSION_DIR in "$SUBJECT_DIR"/ses-*; do
+        # Get session number.
+        SESSION=$(basename "$SESSION_DIR")
+
+        # These sessions should have incomplete/invalid data.
+        if [[ "$SESSION" == "ses-00" ]]; then
+            echo "Skipping $SUB/$SESSION"
+            continue
+        fi
+
+        echo "--------------- Starting $SUB/$SESSION ---------------" | tee -a $LOG_FILE
+
+        # Run processing but make sure that any errors are recorded in the
+        # log file.
+        if [ "$DRYRUN" = true ]; then
+            $HOME/freesurfer.sh --data-dir $DATA_DIR/mri/bids --free-dir $OUTPUT_DIR --sub $SUB --session $SESSION --n-threads $NTHREADS --freesurfer-version $FREESURFER_VERSION --dry-run
+        else
+            $HOME/freesurfer.sh --data-dir $DATA_DIR/mri/bids --free-dir $OUTPUT_DIR --sub $SUB --session $SESSION --n-threads $NTHREADS --freesurfer-version $FREESURFER_VERSION 2>> $LOG_FILE
+        fi
+
+        if [ $? -ne 0 ]; then
+            echo "!!---------- Error processing $SUB/$SESSION ----------!!" | tee -a $LOG_FILE
+            echo "$SUB $SESSION" >> "$FAILED_FILE"
+        fi
+
+        echo "--------------- Done with $SUB/$SESSION ---------------" | tee -a $LOG_FILE
+    done
+done
