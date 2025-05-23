eee1985802bb7e407f9e69db7c729fa09a796d85
Michal Toth <toth.michal@ymail.com>
2025-05-23 15:06:51 +1000

    Update the run master script to include the new scripts and options

--- recon-all-clinical_run/run-recon-all-clinical.sh
+++ recon-all-clinical_run/run-recon-all-clinical.sh
@@ -5,7 +5,7 @@ log () {
 }
 
 # Parse arguments.
-PARSED=$(getopt --options "" --long data-dir:,output-dir:,dry-run,n-threads:,freesurfer-version:,excluded: --name "$0" -- "$@")
+PARSED=$(getopt --options "" --long data-dir:,output-dir:,dry-run,n-threads:,modules:,excluded:,keep-files: --name "$0" -- "$@")
 # Terminate script if failed to parse arguments properly.
 if [[ $? -ne 0 ]]; then
     echo "Error parsing options" >&2
@@ -20,8 +20,9 @@ OUTPUT_DIR=""
 DRYRUN=false
 # Use the max amount of available threads as the default value.
 NTHREADS=`nproc`
-FREESURFER_VERSION="8.0.0"
 EXCLUDED=()
+MODULES=( "freesurfer/8.0.0" "afni/24.3.00" )
+KEEP_FILES_FILE=""
 
 # Extract values from arguments. `--` indicates the end of arguments.
 while true; do
@@ -42,12 +43,16 @@ while true; do
             NTHREADS="$2"
             shift 2
             ;;
-        --freesurfer-version)
-            FREESURFER_VERSION="$2"
+        --modules)
+            IFS=' ' read -r -a MODULES <<< "$2"
             shift 2
             ;;
         --excluded)
             IFS=' ' read -r -a EXCLUDED <<< "$2"
+            shift 2
+            ;;
+        --keep-files)
+            KEEP_FILES_FILE="$2"
             shift 2
             ;;
         --)
@@ -66,10 +71,63 @@ if [[ -z "$OUTPUT_DIR" ]]; then
     OUTPUT_DIR="${DATA_DIR}/mri/bids/derivatives/recon-all-clinical"
 fi
 
+# Check that a file that contains files to keep exists. If no file was provided
+# create a temp file and delete it if the script exits or terminates.
+if [[ -n "$KEEP_FILES_FILE" && ! -f "$KEEP_FILES_FILE" ]]; then
+    echo "The file provided in --keep-files could not be found: ${KEEP_FILES_FILE}"
+    exit 1
+elif [[ -z "$KEEP_FILES_FILE" ]]; then
+    KEEP_FILES_FILE=$(mktemp --tmpdir="$PWD")
+    trap 'rm -f "$KEEP_FILES_FILE"' EXIT INT TERM
+    cat <<EOF > "$KEEP_FILES_FILE"
+contents.zip
+mri/brain.nii.gz
+mri/native.nii.gz
+SUMA/std.141.\${SUB}_\${SESSION}_lh.spec
+SUMA/std.141.\${SUB}_\${SESSION}_rh.spec
+SUMA/std.60.\${SUB}_\${SESSION}_lh.spec
+SUMA/std.60.\${SUB}_\${SESSION}_rh.spec
+SUMA/\${SUB}_\${SESSION}_SurfVol.nii
+EOF
+fi
+
+ # Make sure to check that the modules are available. Check all the requested
+ # modules before terminating the script.
+MODULE_ERROR=false
+for mod in $MODULES; do
+    # Provide a better error message if the module does not exist.
+    if ! module avail $mod 2>&1 | grep -q "$mod"; then
+        echo "Module ${mod} could not be found. Make sure that it is downloaded
+    and that you used 'module use <path/to/neurodesk/containers>' before running
+    this."
+        MODULE_ERROR=true
+    fi
+done
+if [[ "$MODULE_ERROR" = true ]]; then
+    exit 1
+fi
+# Extract versions from the modules.
+for mod in "${MODULES[@]}"; do
+    name="${mod%%/*}"
+    version="${mod##*/}"
+
+    case "$name" in
+        freesurfer)
+            FREESURFER_VERSION="$version"
+            ;;
+        afni)
+            AFNI_VERSION="$version"
+            ;;
+        *)
+            echo "Warning: Undefined module $name"
+            ;;
+    esac
+done
+
 # Set up logging file and a file for failed sessions (for re-running).
 # Make sure to include all the arguments and git info for reproducibility.
 LOG_FILE=$OUTPUT_DIR/run-recon-all-clinical_$(date "+%Y-%m-%d_%H-%M-%S").log
-log "$0 --data-dir $DATA_DIR --output-dir $OUTPUT_DIR --n-threads $NTHREADS --freesurfer-version $FREESURFER_VERSION --excluded ${EXCLUDED[*]}"
+log "$0 --data-dir $DATA_DIR --output-dir $OUTPUT_DIR --n-threads $NTHREADS --modules ${MODULES[*]} --excluded ${EXCLUDED[*]}"
 
 GIT_URL=$(git config --get remote.origin.url)
 if [[ -z "$GIT_URL" ]]; then
@@ -113,9 +171,13 @@ for SUBJECT_DIR in "$DATA_DIR"/mri/bids/sub-*; do
         # Run processing but make sure that any errors are recorded in the
         # log file.
         if [ "$DRYRUN" = true ]; then
-            $HOME/freesurfer.sh --data-dir $DATA_DIR/mri/bids --output-dir $OUTPUT_DIR --sub $SUB --session $SESSION --n-threads $NTHREADS --freesurfer-version $FREESURFER_VERSION --dry-run
+            ./freesurfer.sh --data-dir $DATA_DIR/mri/bids --output-dir $OUTPUT_DIR --sub $SUB --session $SESSION --n-threads $NTHREADS --freesurfer-version $FREESURFER_VERSION --dry-run
+            ./suma.sh --data-dir $DATA_DIR/mri/bids --output-dir $OUTPUT_DIR --sub $SUB --session $SESSION --afni-version $AFNI_VERSION --dry-run
+            ./cleanup.sh --data-dir $DATA_DIR/mri/bids --output-dir $OUTPUT_DIR --sub $SUB --session $SESSION --keep-files $KEEP_FILES_FILE --dry-run
         else
-            $HOME/freesurfer.sh --data-dir $DATA_DIR/mri/bids --output-dir $OUTPUT_DIR --sub $SUB --session $SESSION --n-threads $NTHREADS --freesurfer-version $FREESURFER_VERSION 2>> $LOG_FILE
+            ./freesurfer.sh --data-dir $DATA_DIR/mri/bids --output-dir $OUTPUT_DIR --sub $SUB --session $SESSION --n-threads $NTHREADS --freesurfer-version $FREESURFER_VERSION 2>> $LOG_FILE
+            ./suma.sh --data-dir $DATA_DIR/mri/bids --output-dir $OUTPUT_DIR --sub $SUB --session $SESSION --afni-version $AFNI_VERSION 2>> $LOG_FILE
+            ./cleanup.sh --data-dir $DATA_DIR/mri/bids --output-dir $OUTPUT_DIR --sub $SUB --session $SESSION --keep-files $KEEP_FILES_FILE 2>> $LOG_FILE
         fi
 
         if [ $? -ne 0 ]; then
