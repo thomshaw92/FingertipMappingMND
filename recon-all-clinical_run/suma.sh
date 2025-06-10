8b57a7687d1254dc92119239fd91174e1995e3b4
Michal Toth <toth.michal@ymail.com>
2025-06-10 12:25:50 +1000

    Add commands for creating CSF and WM masks

--- recon-all-clinical_run/suma.sh
+++ recon-all-clinical_run/suma.sh
@@ -63,7 +63,7 @@ fi
 
 # Check that required arguments are present. If not terminate the script.
 if [[ -z "$SUB" || -z "$SESSION" ]]; then
-    echo "Error: Both --sub and --sess/--session are required"
+    echo "ERROR: Both --sub and --sess/--session are required"
     exit 1
 fi
 
@@ -75,9 +75,8 @@ fi
 MODULE=afni/$AFNI_VERSION
 # Provide a better error message if the module does not exist.
 if ! module avail $MODULE 2>&1 | grep -q "$MODULE"; then
-    echo "Module ${MODULE} could not be found. Make sure that it is downloaded
-and that you used 'module use <path/to/neurodesk/containers>' before running
-this."
+    echo "ERROR: Module ${MODULE} could not be found. Make sure that it is downloaded
+and that you used 'module use <path/to/neurodesk/containers>' before running this."
     exit 1
 fi
 module load $MODULE
@@ -85,12 +84,30 @@ module load $MODULE
 # Where output will be stored.
 RESULT_DIR=$OUTPUT_DIR/output/$SUB/$SESSION
 
+# Regions for CSF mask.
+CSF_REGIONS="\
+Left-Lateral-Ventricle,\
+Right-Lateral-Ventricle"
+
+# Regions for WM mask (based on what AFNI selects in @SUMA_Make_Spec_FS).
+WM_REGIONS="\
+Left-Cerebral-White-Matter,\
+Right-Cerebral-White-Matter,\
+CC_Posterior,\
+CC_Mid_Posterior,\
+CC_Central,\
+CC_Mid_Anterior,\
+CC_Anterior"
+
 if [[ "$DRYRUN" = true ]]; then
     SUMMARY="
     mkdir -p $RESULT_DIR/SUMA
     mkdir -p $RESULT_DIR/orig
     cp $RESULT_DIR/mri/native.mgz $RESULT_DIR/mri/orig.mgz
     @SUMA_Make_Spec_FS -NIFTI -fspath $RESULT_DIR -sid \"${SUB}_${SESSION}\"
+
+    3dcalc -a \"$RESULT_DIR/SUMA/aparc+aseg.nii<${WM_REGIONS}>\" -datum byte -expr 'step(a)' -prefix $RESULT_DIR/SUMA/fs_custom_wm.nii.gz
+    3dcalc -a \"$RESULT_DIR/SUMA/aparc+aseg.nii<${CSF_REGIONS}>\" -datum byte -expr 'step(a)' -prefix $RESULT_DIR/SUMA/fs_custom_csf.nii.gz
     "
 
     echo "$SUMMARY"
@@ -100,4 +117,18 @@ else
     mkdir -p $RESULT_DIR/orig
     cp $RESULT_DIR/mri/native.mgz $RESULT_DIR/mri/orig.mgz
     @SUMA_Make_Spec_FS -NIFTI -fspath $RESULT_DIR -sid "${SUB}_${SESSION}"
+
+    # @SUMA_Make_Spec_FS creates this file and inserts a table that maps
+    # values/label numbers to labels. This makes it possible to select regions
+    # based on labels. For the commands used see
+    # '3dinfo <path/to/SUMA/folder>/aparc+aseg.nii.gz'.
+    if [[ ! -f "$RESULT_DIR/SUMA/aparc+aseg.nii.gz" ]]; then
+        echo "ERROR: Segmentation file aparc+aseg.nii.gz does not exist. '@SUMA_Make_Spec_FS' should have created this."
+        exit 1
+    fi
+
+    # This was largely taken from what AFNI does already - see
+    # '3dinfo <path/to/SUMA/folder>/fs_ap_wm.nii.gz'.
+    3dcalc -a "$RESULT_DIR/SUMA/aparc+aseg.nii<${WM_REGIONS}>" -datum byte -expr 'step(a)' -prefix $RESULT_DIR/SUMA/fs_custom_wm.nii.gz
+    3dcalc -a "$RESULT_DIR/SUMA/aparc+aseg.nii<${CSF_REGIONS}>" -datum byte -expr 'step(a)' -prefix $RESULT_DIR/SUMA/fs_custom_csf.nii.gz
 fi
