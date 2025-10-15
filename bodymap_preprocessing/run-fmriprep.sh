68efa5f2d7ce98d08284d23f82c9215cc83da04b
t-michal <toth.michal@ymail.com>
2025-10-15 11:44:36 +1000

    Add script for longitudinal fastsurfer and amend fmriprep to use longitudinal processing

--- bodymap_preprocessing/run-fmriprep.sh
+++ bodymap_preprocessing/run-fmriprep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Parse arguments.
-PARSED=$(getopt --options "" --long sub:,sess:,session:,data-dir:,fs-dir:,output-dir:,dry-run,n-threads:,fmriprep-version:,fs-license:,fs-version: --name "$0" -- "$@")
+PARSED=$(getopt --options "" --long sub:,sess:,session:,data-dir:,fs-dir:,output-dir:,dry-run,n-threads:,fmriprep-version:,fs-license: --name "$0" -- "$@")
 # Terminate script if failed to parse arguments properly.
 if [[ $? -ne 0 ]]; then
     echo "Error parsing options" >&2
@@ -16,11 +16,12 @@ SESSION=""
 DATA_DIR="."
 OUTPUT_DIR=""
 DRYRUN=false
-FMRIPREP_VERSION="25.1.3"
+# A bug was introduced in 25.1.0 that stretched data out of FoV of EPI. It was
+# fixed in 25.2.0. Be careful when using any version between those two.
+FMRIPREP_VERSION="25.2.3"
 # Use the max amount of available threads as the default value.
 NTHREADS=`nproc`
 FS_LICENSE_FILE="./license.txt"
-FS_VERSION="8.1.0"
 FS_DIR=""
 
 # Extract values from arguments. `--` indicates the end of arguments.
@@ -62,10 +63,6 @@ while true; do
 			FS_LICENSE_FILE="$2"
 			shift 2
 			;;
-        --fs-version)
-			FS_VERSION="$2"
-			shift 2
-			;;
         --)
             shift
             break
@@ -79,7 +76,7 @@ done
 
 # Make sure to use the DATA_DIR value that was sepcified in the arguments.
 if [[ -z "$FS_DIR" ]]; then
-    FS_DIR="${DATA_DIR}/derivatives/recon-all/output"
+    FS_DIR="${DATA_DIR}/derivatives/long-fastsurfer/output"
 fi
 if [[ -z "$OUTPUT_DIR" ]]; then
     OUTPUT_DIR="${DATA_DIR}/derivatives/fmriprep/output"
@@ -91,7 +88,7 @@ if [[ -z "$SUB" || -z "$SESSION" ]]; then
     exit 1
 fi
 
-MODULES=( "fmriprep/$FMRIPREP_VERSION" "freesurfer/$FS_VERSION" )
+MODULES=( "fmriprep/$FMRIPREP_VERSION" )
 for mod in "${MODULES[@]}"; do
     # Provide a better error message if the module does not exist.
     if ! module avail $mod 2>&1 | grep -q "$mod"; then
@@ -103,37 +100,67 @@ for mod in "${MODULES[@]}"; do
     module load $mod
 done
 
-export SINGULARITYENV_FS_ALLOW_DEEP=1
-export APPTAINERENV_FS_ALLOW_DEEP=1
-mkdir -p $FS_DIR
-
 # This env variable overwrites whatever is passed as `fs-subjects-dir` below.
 # Setting it to an empty string then uses the argument value below.
 export SINGULARITYENV_SUBJECTS_DIR=""
 export APPTAINERENV_SUBJECTS_DIR=""
 
-# FMRIPREP does not like 'sub-' prefix in subject numbers.
+# FMRIPREP does not like 'sub-' and 'ses-' prefixes.
 subno="${SUB#sub-}"
-fmriprep --skip_bids_validation \
-         --participant-label $subno \
-         --nprocs $NTHREADS \
-         --mem 30720 \
-         --ignore fieldmaps slicetiming \
-         --output-spaces MNI152NLin2009cAsym \
-         --force bbr \
-         --medial-surface-nan \
-         --project-goodvoxels \
-         --skull-strip-fixed-seed \
-         --random-seed 12345 \
-         --fmap-no-demean \
-         --use-syn-sdc warn \
-         --force syn-sdc \
-         --fs-license-file $FS_LICENSE_FILE \
-         --fs-subjects-dir $FS_DIR \
-         -w $OUTPUT_DIR/$SUB/$SESSION/wrk-dir \
-         --write-graph \
-         --notrack \
-         --aggregate-session-reports 1 \
-         $DATA_DIR \
-         $OUTPUT_DIR/$SUB/$SESSION \
-         participant
+sesno="${SESSION#ses-}"
+if [ "$DRYRUN" = true ]; then
+    fmriprep_cmd="
+    fmriprep --skip_bids_validation
+             --participant-label $subno
+             --session-label $sesno
+             --subject-anatomical-reference unbiased
+             --ignore fieldmaps slicetiming
+             --output-spaces MNI152NLin2009cAsym
+             --force bbr
+             --medial-surface-nan
+             --skull-strip-fixed-seed
+             --skull-strip-template OASIS30ANTs
+             --random-seed 12345
+             --bold2anat-dof 12
+             --use-syn-sdc warn
+             --force syn-sdc
+             --fs-license-file $FS_LICENSE_FILE
+             -w $OUTPUT_DIR/$SUB/$SESSION/wrk-dir
+             --write-graph
+             --notrack
+             --aggregate-session-reports 1
+             --fs-subjects-dir $FS_DIR
+             --fs-no-resume
+             --nthreads $NTHREADS
+             $DATA_DIR
+             $OUTPUT_DIR
+             participant
+    "
+    echo "$fmriprep_cmd"
+else
+    fmriprep --skip_bids_validation \
+             --participant-label $subno \
+             --session-label $sesno \
+             --subject-anatomical-reference unbiased \
+             --ignore fieldmaps slicetiming \
+             --output-spaces MNI152NLin2009cAsym \
+             --force bbr \
+             --medial-surface-nan \
+             --skull-strip-fixed-seed \
+             --skull-strip-template OASIS30ANTs \
+             --random-seed 12345 \
+             --bold2anat-dof 12 \
+             --use-syn-sdc warn \
+             --force syn-sdc \
+             --fs-license-file $FS_LICENSE_FILE \
+             -w $OUTPUT_DIR/$SUB/$SESSION/wrk-dir \
+             --write-graph \
+             --notrack \
+             --aggregate-session-reports 1 \
+             --fs-subjects-dir $FS_DIR \
+             --fs-no-resume \
+             --nthreads $NTHREADS \
+             $DATA_DIR \
+             $OUTPUT_DIR \
+             participant
+fi
